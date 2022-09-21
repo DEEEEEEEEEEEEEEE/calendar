c7bdf6e2f5ab8a7d418b362667dd4fdbef88e7fc
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2022-09-21 18:38:40 +0900

    Add files via upload

--- jam/app.js
+++ jam/app.js
@@ -1,4 +1,4 @@
-const viewCalender = document.querySelector(".button");
+const viewCalender = document.querySelector(".dropdown-button");
 const view_m = document.querySelector(".dropdown-content");
 
 function clickedViewCalender() {
@@ -11,3 +11,35 @@ function bluredViewCalender() {
 
 viewCalender.addEventListener("click", clickedViewCalender);
 viewCalender.addEventListener("blur", bluredViewCalender);
+
+const arrayWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+for (let w=0;w<arrayWeek.length;w++) {
+    const newSpan = document.createElement("span");
+    newSpan.innerHTML = arrayWeek[w];
+    const r4 = document.querySelector("#row4");
+    r4.appendChild(newSpan);
+}
+
+function days() {
+    for (let i=28;i<32;i++) {
+        const row5 = document.querySelector("#row5");
+        const newDiv = document.createElement("div");
+        newDiv.innerHTML = i;
+        row5.appendChild(newDiv);
+    }
+    
+    for (let j=1;j<31;j++) {
+        const row5 = document.querySelector("#row5");
+        const newDiv = document.createElement("div");
+        newDiv.innerHTML = j;
+        row5.appendChild(newDiv);
+    }
+
+    const row5 = document.querySelector("#row5");
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = "1";
+    row5.appendChild(newDiv);
+}
+
+days();
