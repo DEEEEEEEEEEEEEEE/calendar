7dddb1c114a4a9353af2d40b370d1ed0df17da50
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2022-09-23 15:06:48 +0900

    Add files via upload
    1단계 완료

---
+++ jam/Days.js
@@ -0,0 +1,18 @@
+for (let i=28;i<32;i++) {
+    const row5 = document.querySelector("#row5");
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = i;
+    row5.appendChild(newDiv);
+}
+    
+for (let j=1;j<31;j++) {
+    const row5 = document.querySelector("#row5");
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = j;
+    row5.appendChild(newDiv);
+}
+
+const row5 = document.querySelector("#row5");
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "1";
+row5.appendChild(newDiv);
